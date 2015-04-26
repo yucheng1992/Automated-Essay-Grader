3177432cca224d293eadcebc23fb96a5426bda56
yl2612 <yl2695@nyu.edu>
2015-04-26 01:13:56 -0400

    complete most parts of combineFeatures.py

--- combineFeatures.py
+++ combineFeatures.py
@@ -4,6 +4,7 @@ import pickle
 from parseArticle import *
 from collections import Counter
 from util import *
+from operator import itemgetter
 
 
 class combineFeatures():
@@ -11,28 +12,45 @@ class combineFeatures():
     
     def __init__(self):
         self.spellcheckerFileName = "bigram.pkl"
-        self.posFileName = "featuresPos.pkl"
-        self.wordsFileName = "featureWords.pkl"
+        self.posFileName = "featuresPosEssaySet1.pkl"
+        self.wordsFileName = "featureWordsEssaySet1.pkl"
+        self.missSpellFileName = "missSpellingCount.pkl"
+        self.dataFileName = "training_set_rel3.tsv"
         self.wordNumber, self.sentenceNumber, self.averageWordLength, self.clauseWordNumber = self.readFeaturesFromParseArticle()
-        self.spellData, self.posData, self.wordsData = self.readFile()
+        self.posData, self.missSpellData = self.readFile()
+
+    
+    def returnDomain1Score(self):
+        """select all the domain1 scores from the dataset"""
+        
+        df = pd.DataFrame
+
+        try:
+            df = pd.DataFrame.from_csv(self.dataFileName, sep="\t")
+        except Exception:
+            print "Cannot open the data file due to the exception:", sys.exc_info()[0]
+
+        return df["domain1_score"].tolist()
 
     def readFile(self):
         """Read data from specific files"""
         try:
-            fSpell = open(self.spellcheckerFileName, "rb")
-            spellData = pickle.load(fSpell)
+            # fSpell = open(self.spellcheckerFileName, "rb")
+            # spellData = pickle.load(fSpell)
             
             fPos = open(self.posFileName, "rb")
             posData = pickle.load(fPos)
 
-            fWords = open(self.wordsFileName, "rb")
-            wordsData = pickle.load(fWords)
+            fMiss = open(self.missSpellFileName, "rb")
+            missData = pickle.load(fMiss)
+            # fWords = open(self.wordsFileName, "rb")
+            # wordsData = pickle.load(fWords)
             
         except Exception, e:
             print "Cannot open file due to the exception: "
             print e
             raise
-        return spellData, posData, wordsData
+        return posData, missData
 
     
     def readFeaturesFromParseArticle(self):
@@ -44,13 +62,19 @@ class combineFeatures():
 
     def combineAllFeatures(self):
         """combine all the articless features together"""
+        
+        scores = self.returnDomain1Score()
+    
         combineData = []
-        for i in range(len(self.wordNumber)):
-            featureCounter = self.posData[i] + self.wordsData[i]
-            featureCounter["wordNumber"] = self.wordNumber[i]
-            featureCounter["sentenceNumber"] = self.sentenceNumber[i]
-            featureCounter["averageWordLength"] = self.averageWordLength[i]
-            featureCounter["clauseWordNumber"] = self.clauseWordNumber[i]
+        for i in range(len(self.posData)):
+            # featureCounter = self.posData[i] + self.wordsData[i]
+            featureCounter = self.posData[i]
+            featureCounter["wordNumber"] = self.wordNumber[0][i]
+            featureCounter["sentenceNumber"] = self.sentenceNumber[0][i]
+            featureCounter["averageWordLength"] = self.averageWordLength[0][i]
+            featureCounter["clauseWordNumber"] = self.clauseWordNumber[0][i]
+            featureCounter["missSpelling"] = self.missSpellData[i]
+            featureCounter["score"] = scores[i]
             combineData.append(featureCounter)
         return combineData
 
@@ -60,13 +84,20 @@ class combineFeatures():
         features = self.combineAllFeatures()
         
         totalCounter = Counter()
+    
 
         for feature in features:
-            totalCounter = increment(totalCounter, 0,feature)
+            totalCounter = increment2(totalCounter, 0,feature)
+        
 
         for feature in features:
-            feature += totalCounter
-        return features
+            feature = increment2(feature, 1, totalCounter)
+            print feature    
+        
+        df = pd.DataFrame(features)
+        
+        df.to_csv("essaySet1.csv")
+       
 
 if __name__ == '__main__':
     cf = combineFeatures()
