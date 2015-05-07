a0970f2fe92642ccdd25191b07d22517eb47bee8
yl2612 <yl2695@nyu.edu>
2015-05-06 20:41:41 -0400

    add selecting parameters part in random forest regression model

--- randomForestRegression.py
+++ randomForestRegression.py
@@ -45,32 +45,69 @@ class randomForestRegression(object):
 
     def randomForestRegressionModel(self, essaySetNumber):
         """Train the random forest model using the training data"""
+        
         trainingData, validationData = self.readTrainingValidationData()
-        validationScore = self.readVaidationScores(essaySetNumber)
-        trainingScore = trainingData["score"]
-        trainingData = trainingData.drop(["Unnamed: 0", "score"], 1)
-        validationData = validationData.drop("Unnmaed: 0", 1)
+        if essaySetNumber != 2:
+            validationDomainOneScore = self.readVaidationScores(essaySetNumber)
+        else:
+            validationDomainOneScore = self.readVaidationScores(essaySetNumber)[0::2]
+            validationDomainTwoScore = self.readVaidationScores(essaySetNumber)[1::2]
+        
+        maxScore = 0
+        maxDepth = 0
+        minLeaf = 0
+        minSplit = 0
+        
+        trainingData = trainingData.drop(["Unnamed: 0", "domain1_score"], 1)
+        validationData = validationData.drop(["Unnamed: 0", "domain1_score"], 1)
+        trainingDomainOneScore = trainingData["domain1_score"]
+        if essaySetNumber == 2:
+            trainingDomainTwoScore = trainingData["domain2_score"]
+            validationData = validationData.drop("domain2_score", 1)
 
-        # train model
-        clf = RandomForestRegressor()
-        clf = clf.fit(trainingData, trainingScore)
-       
-        # make predictions
-        predictScores = clf.predict(validationData)
-        predictScores = map(round, predictScores)
-        predictScores = map(int, predictScores) 
-        return predictScores, validationScore
+        # train model and select best parameters
+        for depth in range(6, 50, 10):
+            for split in range(1, 20, 5):
+                for leaf in range(20, 80, 10):
+                    clf = RandomForestRegressor(max_depth=depth, min_samples_split=split, min_samples_leaf=leaf)
+                    clf = clf.fit(trainingData, trainingDomainOneScore)
+                        
+                    # make predictions
+                    predictDomainOneScores = clf.predict(validationData)
+                    predictDomainOneScores = map(round, predictDomainOneScores)
+                    predictDomainOneScores = map(int, predictDomainOneScores) 
+                    score = quadratic_weighted_kappa(validationDomainOneScore, predictDomainOneScores)
+                    
+                    if essaySetNumber == 2:
+                        clf = RandomForestRegressor(max_depth=depth, min_samples_split=split, min_samples_leaf=leaf)
+                        clf = clf.fit(trainingData, trainingDomainTwoScore)
+                        
+                        predictDomainTwoScores = clf.predict(validationData)
+                        predictDomainTwoScores = map(round, predictDomainTwoScores)
+                        predictDomainTwoScores = map(int, predictDomainTwoScores)
+                        score = (score + quadratic_weighted_kappa(validationDomainTwoScore, predictDomainTwoScores)) / 2.0
+                    
+                    if score > maxScore:
+                        maxScore = score
+                        maxDepth = depth
+                        minLeaf = leaf
+                        minSplit = split
+
+        return  maxScore, maxDepth, minSplit, minLeaf
 
 
     def evaluatPredictions(self, essaySetNumber):
         """Make evaluations on predictions"""
-        prediction, trueScore = self.randomForestRegressionModel(essaySetNumber) 
-        return quadratic_weighted_kappa(trueScore, prediction)
+        max = self.randomForestRegressionModel(essaySetNumber) 
+        return max
+        # return quadratic_weighted_kappa(trueScore, prediction)
 
 if __name__ == '__main__':
+    f = open("maximalKappaScores.txt", "wb")
     for i in range(1, 9):
-        trainingFile = "trainingTfidfEssaySet" + str(i) + ".csv"
-        testingFile = "testingTfidfEssaySet" + str(i) + ".csv"
-        scoreFile = "validationScores.pkl"
-        rf = randomForestRegression(trainingFile, testingFile, scoreFile)
-        print rf.evaluatPredictions(1)
+        train = "trainingTfidfEssaySet" + str(i) + ".csv"
+        test = "testingTfidfEssaySet" + str(i) + ".csv"
+        model = randomForestRegression(train, test, "validationScores.pkl")
+        max, depth, split, leaf = model.randomForestRegressionModel(i)
+        f.write("Kappa score = %f, max_depth = %d, min_samples_split = %d, min_samples_leaf = %d \n" %(max, depth, split, leaf))
+    f.close()
