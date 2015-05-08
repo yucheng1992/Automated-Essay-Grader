a7baaebffb07b70cb4b1047f72c690186f7e96c4
yl2612 <yl2695@nyu.edu>
2015-05-07 21:28:45 -0400

    add similarity score's main func

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from FeatureExtraction import ExtractPos
 from FeatureExtraction import GenerateArticleFeatures
 from FeatureExtraction import CombineFeatures
 from FeatureExtraction import MisspellingChecker
+from FeatureExtraction import SimilarityScore
 from Model import GradientBoostingRegressionTreeModel
 from Model import RandomForestRegressionModel
 from Util import util
@@ -11,6 +12,7 @@ from Util import SelectValidationScores
 
 if __name__ == '__main__':
     MisspellingChecker.main()
+    SimilarityScore.main()
     FeatureExtraction.main()
     CombineFeatures.main()
     GradientBoostingRegressionTreeModel.main()
